6df7d409d188ef19d67e2945404ed085cdf82a6f
Antonio <a.caristia@gmail.com>
2026-08-19 15:37:15 +0200

    Add Express validation starter

---
+++ activities/tpsi5/feisbuc_express_c/starter/src/validation.js
@@ -0,0 +1,9 @@
+export function validateNewPost(input) {
+  // TODO: riusa il modello dell'Activity B.
+  return { ok: false, error: "todo" };
+}
+
+export function validateLikePatch(input) {
+  // TODO: accetta soltanto { liked: boolean }.
+  return { ok: false, error: "todo" };
+}
