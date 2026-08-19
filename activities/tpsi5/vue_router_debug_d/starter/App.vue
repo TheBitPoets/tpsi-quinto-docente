2df5d73fb1b65f7273780861eaff8b2b4dd5a33d
Antonio <a.caristia@gmail.com>
2026-08-19 22:41:15 +0200

    Add broken internal navigation snippet

---
+++ activities/tpsi5/vue_router_debug_d/starter/App.vue
@@ -0,0 +1,7 @@
+<script setup>
+function goFeed(){
+  // BUG: reload completo e path che ignora il mount /vue/.
+  window.location.assign("/feed");
+}
+</script>
+<template><main><h1>Router debug</h1><button type="button" @click="goFeed">Feed</button><RouterView /></main></template>
