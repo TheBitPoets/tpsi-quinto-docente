f5392377f1a6d4aaf402c33391338a7bc8362fff
Antonio <a.caristia@gmail.com>
2026-08-19 12:53:03 +0200

    Add dynamic Feisbuc post model starter

---
+++ activities/tpsi5/feisbuc_dynamic_c/starter/posts.js
@@ -0,0 +1,12 @@
+export function createPost(author, text) {
+  // TODO: restituisci un nuovo post con:
+  // id stabile, author, text, likes: 0, liked: false.
+  // Usa crypto.randomUUID() per l'id nel browser.
+  return {};
+}
+
+export function toggleLike(posts, targetId) {
+  // TODO: riusa il modello dell'Activity B:
+  // map + object spread, senza mutare direttamente il post target.
+  return posts;
+}
