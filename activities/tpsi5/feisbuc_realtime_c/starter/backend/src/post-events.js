631c8a45c56c8d7716aed23b910e0e4ac7ab799b
Antonio <a.caristia@gmail.com>
2026-08-20 21:27:23 +0200

    Add post event bus starter

---
+++ activities/tpsi5/feisbuc_realtime_c/starter/backend/src/post-events.js
@@ -0,0 +1,14 @@
+import { EventEmitter } from "node:events";
+
+export function createPostEvents() {
+  const emitter = new EventEmitter();
+  return {
+    publish(event) {
+      emitter.emit("post-event", event);
+    },
+    subscribe(listener) {
+      emitter.on("post-event", listener);
+      return () => emitter.off("post-event", listener);
+    },
+  };
+}
